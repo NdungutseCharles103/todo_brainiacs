d398e72b65b6f9d630875027ecf2cf5f870994e3
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-05-07 19:51:15 +0200

    __

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Form from "./components/form";
+import Header from "./components/Header";
 import TodoList from "./components/todolist";
+import './App.css'
 
 function App() {
+
+  
   return (
-    <div className="">
-      <Form />
-      <TodoList />
+    <div className="flex flex-col w-full App h-screen">
+      <Header />
+      <div className="flex flex-col bg-white w-2/3  mx-auto p-5 items-center">
+        <Form />
+        <TodoList />
+      </div>
     </div>
   );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BiPlus } from 'react-icons/bi'
+import '../App.css'
 
 function Form() {
   return (
-    <div>Form</div>
-  )
+    <form className="w-3/4 mx-auto rounded-3xl flex items-center justify-between bg-sky-300">
+      <input
+        className="w-[80%] outline-none border-none mx-auto bg-transparent text-white text-xl px-2"
+        type="text"
+        placeholder="Add a Todo"
+      />
+      <div className=" rounded-full p-4 bg-sky-600">
+        <BiPlus className=" text-white" />
+      </div>
+    </form>
+  );
 }
 
 export default Form
