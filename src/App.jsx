1cc90847bb019675b2bbbb64b63c171c202830d2
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-05-07 19:58:17 +0200

    some

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/form";
 import Header from "./components/Header";
 import TodoList from "./components/todolist";
 import './App.css'
 
 function App() {
+  const [todos, setTodos]= useState([])
 
-  
   return (
     <div className="flex flex-col w-full App h-screen">
       <Header />
       <div className="flex flex-col bg-white w-2/3  mx-auto p-5 items-center">
-        <Form />
-        <TodoList />
+        <Form todos={todos} setTodos={setTodos} />
+        <TodoList todos={todos} setTodos={setTodos} />
       </div>
     </div>
   );
