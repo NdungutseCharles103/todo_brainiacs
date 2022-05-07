8af9de2b656acdcb36ed0089b10ddcc868fae5a5
Isimbilysa <lysaisimbi7@gmail.com>
2022-05-07 19:58:49 +0200

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <h1>PANGA</h1>
+      <button className="add-btn">Add Todo <FontAwesomeIcon icon="fa-solid fa-list-check" />
+      </button>
+    </div>
   )
 }
 
